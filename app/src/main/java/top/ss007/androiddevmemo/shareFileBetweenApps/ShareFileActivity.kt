e10e01d5eaa9e9c15018edfc1c0a634982737f7d
Ben.Wang <ben.wang@homecreditcfc.cn>
2019-11-22 17:31:24 +0800

    feature：add share file demo

--- app/src/main/java/top/ss007/androiddevmemo/shareFileBetweenApps/ShareFileActivity.kt
+++ app/src/main/java/top/ss007/androiddevmemo/shareFileBetweenApps/ShareFileActivity.kt
@@ -2,34 +2,35 @@ package top.ss007.androiddevmemo.shareFileBetweenApps
 
 import android.app.Activity
 import android.content.Intent
+import android.graphics.BitmapFactory
 import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.Button
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import top.ss007.androiddevmemo.R
 import top.ss007.androiddevmemo.utils.FileUtils
 import java.io.File
-import java.io.FileInputStream
 import java.io.FileNotFoundException
 import java.io.FileOutputStream
 
 
 class ShareFileActivity : AppCompatActivity() {
 
+    private lateinit var ivImage: ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_share_file)
+        ivImage = findViewById(R.id.iv_display)
         findViewById<Button>(R.id.btn_share_file).setOnClickListener(listener)
         findViewById<Button>(R.id.btn_get_file_from_other_app).setOnClickListener(listener)
-
-
     }
 
     private val listener = View.OnClickListener { v ->
         when (v?.id) {
             R.id.btn_share_file -> {
-
+                installApk(this@ShareFileActivity)
             }
             R.id.btn_get_file_from_other_app -> {
                 val intent = Intent(ACTION_SHARE_FILE).apply {
@@ -38,41 +39,28 @@ class ShareFileActivity : AppCompatActivity() {
                 startActivityForResult(intent, REQUEST_GET_FILE)
             }
         }
-
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         if (resultCode == Activity.RESULT_OK && requestCode == REQUEST_GET_FILE) {
-            // Get the file's content URI from the incoming Intent
             data?.data?.also { returnUri ->
-                /*
-                 * Try to open the file for "read" access using the
-                 * returned URI. If the file isn't found, write to the
-                 * error log and return.
-                 */
-                val inputText  = try {
+                val inputText = try {
                     contentResolver.openFileDescriptor(returnUri, "r")
                 } catch (e: FileNotFoundException) {
-                    e.printStackTrace()
                     Log.e("MainActivity", "File not found.")
                     return
                 }
-
-                // Get a regular file descriptor for the file
                 val fd = inputText?.fileDescriptor
-                val inputStream=FileInputStream(fd)
-                val outStream=FileOutputStream(File(this@ShareFileActivity.getExternalFilesDir(null),"temp.txt"))
-
+                ivImage.setImageBitmap(BitmapFactory.decodeFileDescriptor(fd))
             }
         }
     }
 
-
-    private fun installApk(file: File, act: Activity) {
+    private fun installApk(act: Activity) {
         val intent = Intent(Intent.ACTION_VIEW).apply {
             setDataAndType(
-                FileUtils.getUriForFile(act, file),
+                FileUtils.getUriForFile(act, copyFileToLocalStorage(act)),
                 "application/vnd.android.package-archive"
             )
             flags = Intent.FLAG_GRANT_READ_URI_PERMISSION
@@ -81,7 +69,21 @@ class ShareFileActivity : AppCompatActivity() {
         startActivity(intent)
     }
 
+
+    private fun copyFileToLocalStorage(act: Activity): File {
+        val dir = act.getExternalFilesDir(null)
+        val file = File(dir, "appProvider.apk")
+        val inputStream = resources.openRawResource(R.raw.app_provider)
+        val data = ByteArray(inputStream.available())
+        inputStream.read(data)
+        FileOutputStream(file).use {
+            it.write(data)
+        }
+        return file
+    }
+
     companion object {
+        const val TAG = "ShareFileActivity"
         const val REQUEST_GET_FILE = 1
         const val ACTION_SHARE_FILE = "top.ss007.dev.memo.SHARE"
     }
