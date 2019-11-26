8bdf0b7d2e8ea0690c305c0334b366ffd8f62854
Ben.Wang <ben.wang@homecreditcfc.cn>
2019-11-26 17:48:53 +0800

    feature：add jetpack demo

--- libraries/viewbinding/src/main/java/top/ss007/viewbinding/DescriptionActivity.kt
+++ libraries/viewbinding/src/main/java/top/ss007/viewbinding/DescriptionActivity.kt
@@ -1,28 +1,21 @@
 package top.ss007.viewbinding
 
-import android.content.res.Configuration
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import top.ss007.viewbinding.databinding.ActivityDescriptionBinding
+import java.text.SimpleDateFormat
 import java.util.*
 
 
 class DescriptionActivity : AppCompatActivity() {
-    private lateinit var  binding: ActivityDescriptionBinding
+    private lateinit var binding: ActivityDescriptionBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding=ActivityDescriptionBinding.inflate(layoutInflater)
+        binding = ActivityDescriptionBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.tvDescription.text="介绍如何使用view binding功能"
-
-    }
-
-    override fun onConfigurationChanged(newConfig: Configuration) {
-        super.onConfigurationChanged(newConfig)
-        if (newConfig.orientation==Configuration.ORIENTATION_LANDSCAPE) {
-            binding.tvDisplayDate?.text=Date().toString()
-        }
+        binding.tvDescription.text = "关关雎鸠,在河之洲.窈窕淑女,君子好逑."
+        binding.tvDisplayDate?.text = SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(Date())
     }
 }
